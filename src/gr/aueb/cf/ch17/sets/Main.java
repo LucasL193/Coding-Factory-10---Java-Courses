a278fd1b14e651dfbb422b3b239cb1afa6cfdd17
LucasL <lloudis193@gmail.com>
2026-06-08 20:28:28 +0300

    Added lists and sets in Ch17

---
+++ src/gr/aueb/cf/ch17/sets/Main.java
@@ -0,0 +1,26 @@
+package gr.aueb.cf.ch17.sets;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Set<String> bag = new HashSet<>();
+
+        bag.add("apple");
+        bag.add("apple"); // No duplicates allowed
+        bag.add("banana");
+        bag.add("orange");
+
+        if (bag.contains("apple")) {
+            bag.remove("apple");
+        } else {
+            System.out.println("Not found");
+        }
+
+        // Traverse
+        bag.forEach(System.out::println);
+        bag.removeIf(f -> f.startsWith("b")); // predicate - boolean function
+    }
+}
