d70b47427a54f48e52f661227ebc45023f9aeec7
LucasL <lloudis193@gmail.com>
2026-06-08 20:44:06 +0300

    Added LifoParking

---
+++ src/gr/aueb/cf/ch17/lifo_parking/LifoParking.java
@@ -0,0 +1,41 @@
+package gr.aueb.cf.ch17.lifo_parking;
+
+import java.time.LocalDateTime;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LifoParking {
+   private final Deque<String> parking = new LinkedList<>();
+
+   public LifoParking() {}
+
+   public List<String> getParking() {
+       return List.copyOf(parking); // unmodifiable copy
+   }
+
+   public void addCar(String car) {
+       parking.push(car);
+       System.out.println(LocalDateTime.now() + ": Αρ. πινακίδας " + car);
+   }
+
+   public String removeCar() {
+       String car = parking.pop();
+       System.out.println(LocalDateTime.now() + ", Έξοδος, Αρ. Πινακίδας= " + car);
+       return car;
+   }
+
+   public boolean isEmpty() {
+       return parking.isEmpty();
+   }
+
+   public int size() {
+       return parking.size();
+   }
+
+   public void traverse() {
+       parking.forEach(System.out::println);
+   }
+
+
+}
