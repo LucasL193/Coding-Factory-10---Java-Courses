17ebcc1f5b2cf66f411785f2ecff3c5664e1520f
LucasL <lloudis193@gmail.com>
2026-06-28 15:56:37 +0300

    Add account management functionality with DAO, service, and controller layers

--- src/gr/aueb/cf/ch18/bankapp_mysql/bankapp/core/DBHelper.java
+++ src/gr/aueb/cf/ch18/bankapp_mysql/bankapp/core/DBHelper.java
@@ -7,18 +7,21 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DBHelper {
-
     private static final HikariDataSource datasource;
 
     static {
-
         HikariConfig config = new HikariConfig();
 
         // Config DB
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/bankdbdev" +
+//        config.setJdbcUrl("jdbc:mysql://localhost:3306/bankdbdev" +
+//                "?sslMode=preferred&serverTimeZone=UTC&allowPublicKeyRetrieval=true");
+//        config.setUsername("bankuser10");
+//        config.setPassword("C0d1ngF@");
+
+        config.setJdbcUrl("jdbc:mysql://localhost:3307/bankdb" +
                 "?sslMode=preferred&serverTimeZone=UTC&allowPublicKeyRetrieval=true");
-        config.setUsername("bankuser10");
-        config.setPassword("Lucas!(0398");
+        config.setUsername("bankuser");         // MYSQL_USER
+        config.setPassword("C0d1ngF@");         // MYSQL_PASSWORD
 
         datasource = new HikariDataSource(config);
     }
